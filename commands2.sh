471c1c40bace51cc2f93356e0c9521c512451571
Norman Wong <nich19032@gmail.com>
2020-01-30 19:34:09 +1100

    Added some commands on getting help and info on the commands types

--- commands2.sh
+++ commands2.sh
@@ -9,7 +9,9 @@ echo "Your UID is ${UID}"
 # Display the username
 # USER_NAME=`id -un` <- older systax
 USER_NAME=$(id -un) 
+# Alternative: USER_NAME=$(whoami)
 echo "Your username is ${USER_NAME}"
+
 
 # Display if the user is the root user or not
 if [[ "${UID}" -eq 0 ]] # This statement translate to "If UID equal to 0"
@@ -18,3 +20,10 @@ then
 else
     echo 'You are not root'
 fi
+
+# Getting info on the 
+type -a [[ # Expected Output is [[ is a shell keyword
+type -a whoami  # Expected Output is that whoami is a program located in a directory
+
+help [[ # Display a description of the [[ command
+man whoami # Display a manual with descriptions about the program
