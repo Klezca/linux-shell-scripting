621145bcfb7dddbf7d1f9087c003f6f72a9b75cc
Norman Wong <nich19032@gmail.com>
2020-01-30 19:36:09 +1100

    Clean up script and added more explanations to the script

--- commands2.sh
+++ commands2.sh
@@ -16,14 +16,16 @@ echo "Your username is ${USER_NAME}"
 # Display if the user is the root user or not
 if [[ "${UID}" -eq 0 ]] # This statement translate to "If UID equal to 0"
 then
-    echo 'You are root'
+    echo 'You are root' # The UID of a root user is ALWAYS equal to 0
 else
     echo 'You are not root'
 fi
 
-# Getting info on the 
-type -a [[ # Expected Output is [[ is a shell keyword
+# Getting info on the commands
+type -a [[ # Expected Output: [[ is a shell keyword
 type -a whoami  # Expected Output is that whoami is a program located in a directory
+type -a test # Expected output: test is a shell builtin
 
 help [[ # Display a description of the [[ command
 man whoami # Display a manual with descriptions about the program
+help test | less # Display the description of the test command and pipe it to a pager for easier readability
