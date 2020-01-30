96655545d6c4b39ea51a37718966ef98fc335aeb
Norman Wong <nich19032@gmail.com>
2020-01-30 18:34:15 +1100

    Added some scripts to show how to work with variables

--- commands.sh
+++ commands.sh
@@ -4,3 +4,31 @@
 
 # Display 'Hello'
 echo 'Hello'
+
+# Assign a value to a variable
+WORD='script'
+
+# Display that value using the variable
+echo "$WORD"
+
+# Demonstrate that single quotes cause variables to not get expanded.
+echo '$WORD'
+
+# combine the variable with hard-coded text
+echo "This is a shell $WORD"
+
+# Display the contents of the variable using an alternative systax
+echo "This is a shell ${WORD}"
+
+# Append text to the variabe
+echo "${WORD}ing is fun"
+
+# Show how NOT to append text to variable.
+# This doesn't work:
+echo "$WORDing is fun"
+
+# Creare a new variable
+ENDING='ed'
+
+# Combine the two variables.
+echo "This is ${WORD}${ENDING}."
