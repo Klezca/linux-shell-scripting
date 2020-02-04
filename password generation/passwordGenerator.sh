c1da03a7fa32a0fcdbf737ddb162be295e4f4e74
Norman Wong <nich19032@gmail.com>
2020-02-04 18:47:05 +1100

    Fix error with combining password and specialchar variable, and change sha256 to sha512

--- password generation/passwordGenerator.sh
+++ password generation/passwordGenerator.sh
@@ -11,13 +11,13 @@ PASSWORD=$(date +%s%N) # Use the "man date" command to see what %s and %N does
 echo "${PASSWORD}"
 
 # Better password using the first 20 characters of a 256 hash of the the current date/time
-PASSWORD=$(date +%S%N | sha256sum | head -c20)
+PASSWORD=$(date +%S%N | sha512sum | head -c20)
 echo "${PASSWORD}"
 
 # Adding some randoms number to the above before hashing
-PASSWORD=$(date +%S%N${RANDOM} | sha256sum | head -c20)
+PASSWORD=$(date +%S%N${RANDOM} | sha512sum | head -c20)
 echo "${PASSWORD}"
 
 # Append a random special character to the password 
-SPECIALCHAR=${echo '!@#$%^&*()_-=+' | fold -w1 | shuf | head -c1}
+SPECIALCHAR=$(echo '!@#$%^&*()_-=+' | fold -w1 | shuf | head -c1)
 echo "${PASSWORD}${SPECIALCHAR}"
